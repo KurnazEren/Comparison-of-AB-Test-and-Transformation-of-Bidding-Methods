25f2858024c1debd32bbc39f3ccdfc3c63dbf7c5
Eren Kurnaz <114948164+KurnazEren@users.noreply.github.com>
2023-01-24 13:08:18 +0300

    Update AB_Testing_Eren_Kurnaz_Final.py

--- AB_Testing_Eren_Kurnaz_Final.py
+++ AB_Testing_Eren_Kurnaz_Final.py
@@ -37,17 +37,11 @@ def check_df(dataframe, head=5):
 check_df(df_control)
 check_df(df_test)
 
-
-
 df_control["group"] = "control"
 df_test["group"] = "test"
 
 df = pd.concat([df_control,df_test], axis=0,ignore_index=False)
 df.head()
-
-
-
-
 
 #####################################################
 # Defining A/B Test Hypothesis
@@ -58,24 +52,18 @@ df.head()
 # H0 : M1 = M2 (There is no difference between the control group and test group purchasing averages.)
 # H1 : M1!= M2 (There is a difference between the purchasing averages of the control group and test group.)
 
-
 # Step 2: Analyze the purchase (gain) averages for the control and test group
 
 df.groupby("group").agg({"Purchase": "mean"})
-
-
 
 #####################################################
 # Performing Hypothesis Testing
 # #####################################################
 
-
-
 test_stat, pvalue = shapiro(df.loc[df["group"] == "control", "Purchase"])
 print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
 # p-value=0.5891
 # HO cannot be denied. The values of the control group provide the assumption of normal distribution.
-
 
 test_stat, pvalue = levene(df.loc[df["group"] == "control", "Purchase"],
                            df.loc[df["group"] == "test", "Purchase"])
@@ -84,13 +72,11 @@ print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
 # HO cannot be denied. The values of the Control and Test groups provide the assumption of variance homogeneity.
 # Variances are Homogeneous.
 
-
 test_stat, pvalue = ttest_ind(df.loc[df["group"] == "control", "Purchase"],
                               df.loc[df["group"] == "test", "Purchase"],
                               equal_var=True)
 
 print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
-
 
 # p-value=0.3493
 # HO cannot be denied.
